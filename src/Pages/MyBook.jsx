e288e8addb2eada32145cdae4815ec37a2a9e463
unknown <shariar2739@gmail.com>
2025-06-12 01:56:36 +0600

    added my book page

--- src/Pages/MyBook.jsx
+++ src/Pages/MyBook.jsx
@@ -1,10 +1,88 @@
 import React from "react";
 import { useLoaderData } from "react-router";
+import { RiDeleteBin6Line } from "react-icons/ri";
+import { MdOutlineModeEditOutline } from "react-icons/md";
+import { VscPreview } from "react-icons/vsc";
 
 const MyBook = () => {
-  const myAllBook=useLoaderData()
-  console.log(myAllBook);
-  return <div>this is my book page</div>;
+  const myAllBook = useLoaderData();
+  // console.log(myAllBook);
+  // const { Book_Author, Book_Title, Cover_photo, category, upVote } = book;
+  return (
+    <section>
+      <div
+        className="container p-2 mx-auto sm:p-4 h-screen my-16"
+        bis_skin_checked="1"
+      >
+        {/* <h2 className="mb-4 text-2xl font-semibold leading-tight">Contacts</h2> */}
+        <div className="overflow-x-auto" bis_skin_checked="1">
+          <table className="w-full p-6 text-xs text-left whitespace-nowrap">
+            <colgroup>
+              <col className="w-5" />
+              <col />
+              <col />
+              <col />
+              <col />
+              <col />
+              <col className="w-5" />
+            </colgroup>
+            {/* ------------------------  table head --------------------- */}
+            <thead>
+              <tr className="bg-sky-300">
+                <th className="p-3">Photo</th>
+                <th className="p-3">Name</th>
+                <th className="p-3">Author</th>
+                <th className="p-3">category</th>
+                <th className="p-3">Email</th>
+                <th className="p-3">status</th>
+                <th className="p-3">
+                  <span className="sr-only">Edit</span>
+                </th>
+              </tr>
+            </thead>
+            {/* -------------------------  table body -------------------- */}
+            {myAllBook.map((singleBook) => (
+              <tbody className="border-b border-sky-200  ">
+                <tr className="">
+                  <td className=" text-2xl font-medium py-3 ">
+                    <img className="" src={singleBook.Cover_photo} alt="" />
+                  </td>
+                  <td className="px-3 py-2">
+                    <p>{singleBook.Book_Title}</p>
+                  </td>
+                  <td className="px-3 py-2">
+                    <span>{singleBook.Book_Author}</span>
+                  </td>
+                  <td className="px-3 py-2">
+                    <p>{singleBook.category}</p>
+                  </td>
+                  <td className="px-3 py-2">
+                    <p>{singleBook.email}</p>
+                  </td>
+                  <td className="px-3 py-2">
+                    <p>{singleBook.status}</p>
+                  </td>
+                  <td className="px-3 py-2">
+                    <div className="flex gap-5 items-center">
+                      <button className="cursor-pointer">
+                        <RiDeleteBin6Line size={20} />
+                      </button>
+                      <button className="cursor-pointer">
+                        <MdOutlineModeEditOutline size={20} />
+                      </button>
+                      <button className="cursor-pointer">
+                        <VscPreview size={20} />
+                      </button>
+                    </div>
+                  </td>
+                </tr>
+              </tbody>
+            ))}
+          </table>
+        </div>
+      </div>
+    </section>
+  );
 };
 
 export default MyBook;
--- src/components/Navbar/CardContainer/CardShower.jsx
+++ src/components/Navbar/CardContainer/CardShower.jsx
@@ -23,9 +23,20 @@ const CardShower = ({ book }) => {
             <p className=" text-gray-400">
               <span className="font-semibold">Author</span> : {Book_Author}
             </p>
-            <p className=" text-gray-400">
-              <span className="font-semibold">category</span> : {category}
-            </p>
+            <div className="flex justify-between items-center ">
+              <p className=" text-gray-400">
+                <span className="font-semibold">category</span> : {category}
+              </p>
+              <button
+                type="submit"
+                className="relative z-10 px-4 py-1 btn btn-sm    bg-base-100 overflow-hidden group shadow-xl transition-all duration-300 hover:text-white"
+              >
+                <span className="relative z-10">View More</span>
+
+                {/* Background hover effect */}
+                <span className="absolute left-0 top-0 h-full w-full bg-emerald-500 scale-0 group-hover:scale-150 transition-transform duration-700 ease-out rounded-full z-0"></span>
+              </button>
+            </div>
           </div>
         </div>
       </div>
